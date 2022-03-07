a7fc9550445c8b777a7ae584175cbd61ddc9683c
samuelnfavero <faveronsamuel@gmail.com>
2022-03-07 10:35:52 -0300

    feat:save endpoint development in RebelController

--- src/main/java/com/starwarsresistence/starWarsResistence/gateways/controllers/RebelController.java
+++ src/main/java/com/starwarsresistence/starWarsResistence/gateways/controllers/RebelController.java
@@ -1,5 +1,22 @@
 package com.starwarsresistence.starWarsResistence.gateways.controllers;
 
+import com.starwarsresistence.starWarsResistence.domains.Rebel;
+import com.starwarsresistence.starWarsResistence.usecases.CreateRebel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("starwarsresistence")
 public class RebelController {
+
+    private CreateRebel createRebel;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Rebel save(Rebel rebel){
+        return createRebel.execute(rebel);
+    }
 }
